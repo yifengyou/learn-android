0ffc0b093c3992cd86b98874c5ee6409adb503fd
yifengyou <842056007@qq.com>
2017-11-09 10:01:48 +0800

    20171109-02

---
+++ git-init.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+git config --global user.name "yifengyou"
+git config --global user.email "842056007@qq.com"
+
--- repo-init.sh
+++ repo-init.sh
@@ -3,5 +3,8 @@ which repo &> /dev/null
 if [ $? != 0 ];then
     cp repo /usr/bin/
 fi
-repo init -u https://aosp.tuna.tsinghua.edu.cn/platform/manifest -b android-2.3.1_r1
+repo init -u https://android.googlesource.com/platform/manifest
+cd .repo/manifests/ && git branch -a
+echo "choose a branch!"
+#repo init -u https://aosp.tuna.tsinghua.edu.cn/platform/manifest -b android-2.3.1_r1
 #repo init -u git://github.com/mitwo-dev/platform_manifest -b android-2.3.1_r1
